39735f6eee8dd4f9ed9b9ca8abae020239abaa77
jwfife <jwf126@gmail.com>
2024-07-10 10:57:06 -0400

    Adds search functionality to Google Maps page

--- app/src/main/java/com/example/studybuddy/MapsActivity.java
+++ app/src/main/java/com/example/studybuddy/MapsActivity.java
@@ -3,7 +3,10 @@ package com.example.studybuddy;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.widget.SearchView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,7 +16,13 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.studybuddy.databinding.ActivityMapsBinding;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap myMap;
+    private SearchView mapSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +31,49 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         com.example.studybuddy.databinding.ActivityMapsBinding binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        mapSearchView = findViewById(R.id.mapSearch);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         assert mapFragment != null;
+
+        mapSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                //Get and store user input
+                String location = mapSearchView.getQuery().toString();
+                List<Address> addressList = null;
+
+                if (location != null){
+
+                    Geocoder geocoder = new Geocoder(MapsActivity.this);
+
+                    try {
+                        addressList = geocoder.getFromLocationName(location, 1);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    assert addressList != null;
+                    Address address = addressList.get(0);
+                    LatLng latLing = new LatLng(address.getLatitude(), address.getLongitude());
+                    myMap.addMarker(new MarkerOptions().position(latLing).title(location));
+                    myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLing, 10));
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
         mapFragment.getMapAsync(this);
+
     }
 
     /**
@@ -41,11 +88,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
 
+        myMap = googleMap;
         float zoomLevel = 18;
-        // Add a marker in Sydney and move the camera
-        LatLng worcesterStateUni = new LatLng(42.2681, -71.8443);
-        googleMap.addMarker(new MarkerOptions().position(worcesterStateUni).title("Marker at Worcester State University"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(worcesterStateUni));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(worcesterStateUni, zoomLevel));
+        // Add a marker in Worcester State and move the camera
+        //LatLng worcesterStateUni = new LatLng(42.2681, -71.8443);
+        //myMap.addMarker(new MarkerOptions().position(worcesterStateUni).title("Marker at Worcester State University"));
+        //myMap.moveCamera(CameraUpdateFactory.newLatLng(worcesterStateUni));
+        //myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(worcesterStateUni, zoomLevel));
     }
 }
