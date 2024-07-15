b8de5cc1d3574089b50a9678cbd1d4c572b38d20
jwfife <jwf126@gmail.com>
2024-07-15 16:50:12 -0400

    Accurately grabs current location and represents it with a marker on map

--- app/src/main/java/com/example/studybuddy/MapsActivity.java
+++ app/src/main/java/com/example/studybuddy/MapsActivity.java
@@ -1,13 +1,21 @@
-package com.example.studybuddy;
+   package com.example.studybuddy;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.SearchView;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -16,13 +24,23 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.studybuddy.databinding.ActivityMapsBinding;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
 import java.io.IOException;
 import java.util.List;
+import java.lang.System;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap myMap;
     private SearchView mapSearchView;
+    private final int FINE_PERMISSION_CODE = 1;
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
+//    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//            .findFragmentById(R.id.map);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +48,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         com.example.studybuddy.databinding.ActivityMapsBinding binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        //getLastLocation();
 
         mapSearchView = findViewById(R.id.mapSearch);
 
@@ -46,7 +67,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 String location = mapSearchView.getQuery().toString();
                 List<Address> addressList = null;
 
-                if (location != null){
+                if (location != null) {
 
                     Geocoder geocoder = new Geocoder(MapsActivity.this);
 
@@ -74,6 +95,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         mapFragment.getMapAsync(this);
 
+        getLastLocation();
+
+    }
+
+    private void getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_PERMISSION_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null){
+                    currentLocation = location;
+                    //System.out.println(currentLocation);
+                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                    //assert mapFragment != null;
+                    mapFragment.getMapAsync(MapsActivity.this);
+                }
+            }
+        });
+
     }
 
     /**
@@ -86,14 +130,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
+    public void onMapReady(@NonNull GoogleMap myMap) {
 
-        myMap = googleMap;
         float zoomLevel = 18;
+        if (currentLocation!= null) {
+            //System.out.println(currentLocation);
+            LatLng currLocation = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+            myMap.addMarker(new MarkerOptions().position(currLocation).title("My Location"));
+        }
+
         // Add a marker in Worcester State and move the camera
         //LatLng worcesterStateUni = new LatLng(42.2681, -71.8443);
         //myMap.addMarker(new MarkerOptions().position(worcesterStateUni).title("Marker at Worcester State University"));
         //myMap.moveCamera(CameraUpdateFactory.newLatLng(worcesterStateUni));
         //myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(worcesterStateUni, zoomLevel));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == FINE_PERMISSION_CODE){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                //SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                getLastLocation();
+            } else {
+                Toast.makeText(this, "Location permission is denied. Please allow the permission", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
