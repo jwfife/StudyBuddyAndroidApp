60d011f53b9ffad5dd7d5312baf145eb0a416fd3
jwfife <jwf126@gmail.com>
2025-03-18 17:04:18 -0400

    Refactor: SignIn for cleaner code

--- app/src/main/java/com/example/studybuddy/SignIn.java
+++ app/src/main/java/com/example/studybuddy/SignIn.java
@@ -15,62 +15,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 /* TODO:
-    - Add forgot password option
+    - Add forgot passwordField option
  */
 public class SignIn extends AppCompatActivity {
 
-    EditText email, password;
-    Button btnLogin;
+    EditText emailField, passwordField;
+    Button loginButton;
     DatabaseHelper DB;
-    ImageView viewingEye;
+    ImageView viewPasswordEye;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
 
-        email = findViewById(R.id.editEmailAddress);
-        password = findViewById(R.id.editPassword);
-        btnLogin = findViewById(R.id.sign_in);
+        emailField = findViewById(R.id.editEmailAddress);
+        passwordField = findViewById(R.id.editPassword);
+        loginButton = findViewById(R.id.sign_in);
         DB = new DatabaseHelper(this);
-        viewingEye = findViewById(R.id.view_password_eye);
+        viewPasswordEye = findViewById(R.id.view_password_eye);
 
-        viewingEye.setOnClickListener(new View.OnClickListener() {
+        viewPasswordEye.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (password.getTransformationMethod().getClass().getSimpleName() .equals("PasswordTransformationMethod")) {
-                    password.setTransformationMethod(new SingleLineTransformationMethod());
-                }
-                else {
-                    password.setTransformationMethod(new PasswordTransformationMethod());
-                }
-
-                password.setSelection(password.getText().length());
+                setViewingEye();
             }
         });
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String user_email = email.getText().toString();
-                String user_pass = password.getText().toString();
+                String userEmail = emailField.getText().toString();
+                String userPassword = passwordField.getText().toString();
 
-                if (user_email.isEmpty() || user_pass.isEmpty()) {
-                    Toast.makeText(SignIn.this, "Please enter all fields", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Boolean checkEmailAndPassword = DB.checkEmailPassword(user_email, user_pass);
-                    if (checkEmailAndPassword) {
-                        Toast.makeText(SignIn.this, "Sign in successful", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(getApplicationContext(), ActionSelection.class);
-
-                        //passes the user's email through the intent to the next activity
-                        intent.putExtra("key", user_email);
-                        startActivity(intent);
-                    }
-                    else {
-                        Toast.makeText(SignIn.this, "Invalid credentials", Toast.LENGTH_SHORT).show();
-                    }
+                if(!isUserPasswordEmpty(userEmail, userPassword)) {
+                    verifyLoginCredentials(userEmail, userPassword);
                 }
             }
         });
@@ -99,5 +78,38 @@ public class SignIn extends AppCompatActivity {
 
 
 
+    }
+    public void setViewingEye() {
+        if (passwordField.getTransformationMethod().getClass().getSimpleName() .equals("PasswordTransformationMethod")) {
+            passwordField.setTransformationMethod(new SingleLineTransformationMethod());
+        }
+        else {
+            passwordField.setTransformationMethod(new PasswordTransformationMethod());
+        }
+
+        passwordField.setSelection(passwordField.getText().length());
+    }
+
+    public Boolean isUserPasswordEmpty(String userEmail, String userPassword) {
+        if (userEmail.isEmpty() || userPassword.isEmpty()) {
+            Toast.makeText(SignIn.this, "Please enter all fields", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return false;
+    }
+
+    public void verifyLoginCredentials(String userEmail, String userPassword) {
+        Boolean checkEmailAndPassword = DB.checkEmailPassword(userEmail, userPassword);
+        if (checkEmailAndPassword) {
+            Toast.makeText(SignIn.this, "Sign in successful", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(getApplicationContext(), ActionSelection.class);
+
+            //passes the user's emailField through the intent to the next activity
+            intent.putExtra("key", userEmail);
+            startActivity(intent);
+        }
+        else {
+            Toast.makeText(SignIn.this, "Invalid credentials", Toast.LENGTH_SHORT).show();
+        }
     }
 }
