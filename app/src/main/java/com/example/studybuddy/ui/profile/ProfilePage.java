d0dad17ee533913db3c84151160e5764db3e2c0a
jwfife <jwf126@gmail.com>
2025-09-12 18:13:44 -0400

    Refactor nav bar button logic for clarity

--- app/src/main/java/com/example/studybuddy/ui/profile/ProfilePage.java
+++ app/src/main/java/com/example/studybuddy/ui/profile/ProfilePage.java
@@ -54,44 +54,7 @@ public class ProfilePage extends AppCompatActivity {
         //sets the user's enrolled course list
         displayUserInfo();
         displayCourseList();
-
-        View viewMessages = findViewById(R.id.messagesPage);
-        viewMessages.setOnClickListener(
-            new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent i = new Intent(ProfilePage.this, MessagesPage.class);
-                    i.putExtra("key", currentUserEmail);
-                    startActivity(i);
-                }
-            }
-        );
-
-        View editProfile = findViewById(R.id.editProfile);
-        editProfile.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent i = new Intent(ProfilePage.this, EditProfile.class);
-                        i.putExtra("key", currentUserEmail);
-                        startActivity(i);
-                    }
-                }
-        );
-
-        View goToHomePage = findViewById(R.id.homePageButton);
-        Bundle extrasToHome = new Bundle();
-        extrasToHome.putString("key", currentUserEmail);
-        goToHomePage.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent i = new Intent(ProfilePage.this, ActionSelection.class);
-                        i.putExtras(extrasToHome);
-                        startActivity(i);
-                    }
-                }
-        );
+        setupButtonListeners();
     }
 
     private void displayUserInfo() {
@@ -122,69 +85,18 @@ public class ProfilePage extends AppCompatActivity {
         courseList.setText(formattedCourses);
     }
 
-//    public void setCourseList() {
-//        //text for course list when no courses are selected
-//        if (DB.getEnrolledCourse(currentUserEmail) == null) {
-//            String noCoursesYet = "No courses yet";
-//            courseList.setText(noCoursesYet);
-//        }
-//        else {
-//            List<String> courseStrArray = getFullCourseStrings();
-//
-//            if (courseStrArray.isEmpty()) {
-//                String noCoursesYet = "No courses yet";
-//                courseList.setText(noCoursesYet);
-//            }
-//            else {
-//                StringBuilder strBuilder = new StringBuilder(courseListStr);
-//                //adds newline character to each course if necessary
-//                for (int k = 0; k < courseStrArray.size(); k++) {
-//                    String courseString = setNewline(courseStrArray, k);
-//                    courseStrArray.set(k, courseString);
-//                    strBuilder.append(courseStrArray.get(k));
-//                }
-//
-//                String finalCourseList = strBuilder.toString();
-//                courseList.setText(finalCourseList);
-//            }
-//        }
-//    }
+    private void setupButtonListeners() {
+        setupNavigation(R.id.messagesPage, MessagesPage.class);
+        setupNavigation(R.id.editProfile, EditProfile.class);
+        setupNavigation(R.id.homePageButton, ActionSelection.class);
+    }
 
-//    public List<String> getFullCourseStrings() {
-//        List<String> fullCourseStrings = new ArrayList<>();
-//        List<String> enrolledCourseIDs = DB.getEnrolledCourse(currentUserEmail);
-//        List<String> enrolledCourseTitles = new ArrayList<>();
-//
-//        //grabs the course title for each course id
-//        for (String id : enrolledCourseIDs
-//        ) {
-//            enrolledCourseTitles.add(DB.getCourseTitles(id));
-//        }
-//
-//        if (!enrolledCourseIDs.isEmpty()) {
-//            for (int i = 0; i < enrolledCourseIDs.size(); i++) {
-//                String fullCourseString = enrolledCourseIDs.get(i) +
-//                        " " + enrolledCourseTitles.get(i);
-//
-//                fullCourseStrings.add(fullCourseString);
-//            }
-//        }
-//        return fullCourseStrings;
-//    }
-//
-//    public String setNewline(List<String> updatedCourseList, int index) {
-//        String courseString;
-//        if (!hasNewline(updatedCourseList, index)) {
-//            courseString = updatedCourseList.get(index) + " \n";
-//        }
-//        else {
-//            courseString = updatedCourseList.get(index);
-//        }
-//        return courseString;
-//    }
-//
-//    public boolean hasNewline(List<String> updatedCourseList, int index) {
-//        return updatedCourseList.get(index).contains("\n");
-//    }
+    private void setupNavigation(int viewID, Class<?> targetActivity) {
+        findViewById(viewID).setOnClickListener(v -> {
+            Intent i = new Intent(ProfilePage.this, targetActivity);
+            i.putExtra("key", currentUserEmail);
+            startActivity(i);
+        });
+    }
 }
 
