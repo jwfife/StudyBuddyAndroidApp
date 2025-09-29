baba67dc95f1d023fd8400ec2345e4ac79f74a20
jwfife <jwf126@gmail.com>
2025-09-29 17:16:36 -0400

    Test: add test for user enrollment

---
+++ app/src/androidTest/java/com/example/studybuddy/TestDatabaseHelper.java
@@ -0,0 +1,51 @@
+package com.example.studybuddy;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import com.example.studybuddy.db.DatabaseHelper;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+public class TestDatabaseHelper {
+
+    private DatabaseHelper DB;
+
+    @Before
+    public void setUp() {
+        Context context = ApplicationProvider.getApplicationContext();
+        DB = DatabaseHelper.getInstance(context);
+
+        DB.onUpgrade(DB.getWritableDatabase(), 1, 1);
+        DB.onCreate(DB.getWritableDatabase());
+    }
+
+    @After
+    public void tearDown() {
+        DB.close();
+    }
+
+    @Test
+    public void testInsertEnrollment() throws SQLException {
+        boolean inserted = DB.insertUserData(
+                "john@example.com",
+                "Password123",
+                "John",
+                "Doe"
+        );
+        assertTrue(inserted);
+
+        assertTrue(DB.checkEmail("john@example.com"));
+        assertTrue(DB.checkEmailPassword("john@example.com", "Password123"));
+        assertEquals("John", DB.getFirstName("john@example.com"));
+        assertEquals("Doe", DB.getLastName("john@example.com"));
+    }
+}
